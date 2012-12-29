91754c67c0dd32c4f2f15d95f237a386c7f3ae1f
Martin Komischke <makom.web@arcor.de>
2012-12-29 15:41:21 +0100

    * Added GlobalAssemblyInfo.cs file for shared version information.

--- Emkay.S3/Properties/AssemblyInfo.cs
+++ Emkay.S3/Properties/AssemblyInfo.cs
@@ -8,9 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Emkay.S3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Emkay.S3")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,16 +19,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("50f7f397-1011-4adc-bdbc-f8a8f3220841")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("Emkay.S3.Tests")]
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Emkay.S3")]
+[assembly: AssemblyCopyright("Copyright (c) 2012")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("5edcdaf292fbdc918e3e6f05160d383312c16018")]

fd0ab8b9b6550bbd5622197488fc4e7592fba359
Martin Komischke <makom.web@arcor.de>
2012-12-30 14:22:06 +0100

    * Added the nuget packaging.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCompany("none")]
+[assembly: AssemblyCompany("Martin Komischke")]
 [assembly: AssemblyProduct("Emkay.S3")]
 [assembly: AssemblyCopyright("Copyright (c) 2012")]
 [assembly: AssemblyVersion("2.0.0.0")]

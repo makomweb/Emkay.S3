a9185ef148fca0141272f147c4a98f81a0da5813
Martin Komischke <makom.web@arcor.de>
2012-12-30 22:06:30 +0100

    * Version set to 0.9.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyCompany("Martin Komischke")]
 [assembly: AssemblyProduct("Emkay.S3")]
 [assembly: AssemblyCopyright("Copyright (c) 2012")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 [assembly: AssemblyInformationalVersion("5edcdaf292fbdc918e3e6f05160d383312c16018")]
